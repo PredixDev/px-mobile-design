865c8965c515b936e2f72f5a5d7961e34c7e9fc0
Jonnie Spratley <jonnie.spratley@ge.com>
2016-04-10 18:03:47 -0700

    fix: updated ui

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const $ = require('gulp-load-plugins')();
 const gulpSequence = require('gulp-sequence');
 const sassdoc = require('sassdoc');
 const importOnce = require('node-sass-import-once');
-
+const filelog = require('gulp-filelog');
 const sassdocOptions = {
   dest: 'docs',
   verbose: true,
@@ -27,26 +27,27 @@ const sassOptions = {
   }
 };
 
-gulp.task('sassdoc', function () {
+gulp.task('sassdoc', function() {
   return gulp.src('sass/**/*.scss')
     .pipe(sassdoc(sassdocOptions));
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
   return gulp.src(['.tmp', 'css'], {
     read: false
   }).pipe($.clean());
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+gulp.task('sass', function() {
+  return gulp.src('./sass/*.scss')
+    .pipe(filelog())
     .pipe($.sass(sassOptions).on('error', $.sass.logError))
     .pipe($.size())
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('autoprefixer', function () {
-  return gulp.src('css/**/*.css')
+gulp.task('autoprefixer', function() {
+  return gulp.src('css/*.css')
     .pipe($.autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
@@ -55,26 +56,40 @@ gulp.task('autoprefixer', function () {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('css', function () {
+gulp.task('css', function() {
   return gulp.src('css/**/*.css')
+    .pipe(filelog())
     .pipe($.sourcemaps.init())
     .pipe($.cssmin())
+    .pipe($.size())
     .pipe($.concat(pkg.name + '.css'))
     .pipe($.sourcemaps.write('.'))
     .pipe($.rename({
       suffix: '.min'
     }))
-    .pipe($.size())
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('autoprefixer:watch', function () {
+gulp.task('autoprefixer:watch', function() {
   gulp.watch('./css/**/*.css', ['autoprefixer']);
 });
 
+
+
+var cssfmt = require('gulp-cssfmt');
+
+gulp.task('cssfmt', function() {
+  return gulp.src(['css/*.css', 'css/*.min.css'])
+    .pipe(filelog())
+    .pipe(cssfmt())
+    .pipe(gulp.dest('dist'));
+});
+
+
+
 gulp.task('watch', ['sass:watch', 'autoprefixer:watch']);
-gulp.task('default', gulpSequence('clean', 'sass', 'autoprefixer', 'css', 'sassdoc'));
+gulp.task('default', gulpSequence('clean', 'sass', 'css', 'autoprefixer', 'cssfmt', 'sassdoc'));
